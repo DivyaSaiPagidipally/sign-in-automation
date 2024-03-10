a7aba7719e7dec10dc291e4964675c22cb36374f
PagidipallyDivyasai <divya.pagidipally@everest.engineering>
2024-03-11 01:04:26 +0530

    Add coustom  key pair ans security

--- main.tf
+++ main.tf
@@ -37,10 +37,10 @@ variable "key_name" {
 }
 
 // Create Key Pair for Connecting EC2 via SSH
-# resource "aws_key_pair" "key_pai" {
-#   key_name   = var.key_name
-#   public_key = tls_private_key.rsa_4096.public_key_openssh
-# }
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
 
 // Save PEM file locally
 resource "local_file" "private_key" {
