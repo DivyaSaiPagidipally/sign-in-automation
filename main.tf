3b2e756c0747dbc0e6005401252bdab7497ab7b4
PagidipallyDivyasai <divya.pagidipally@everest.engineering>
2024-03-10 22:46:21 +0530

    Add default key pair name

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ resource "tls_private_key" "rsa_4096" {
 
 variable "key_name" {
   description = "Name of the SSH key pair"
+  default= "ssh_key_pair"
 }
 
 // Create Key Pair for Connecting EC2 via SSH
